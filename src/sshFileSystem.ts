SchoofsKelvin/vscode-sshfs

commit 201a9ad4ee4b890730b201c79d2fdeaa459a69d9

Fix bug where saving a file resets the permissions (when owner/root at least)

src/sshFileSystem.ts
@@ -71,9 +71,10 @@ export class SSHFileSystem implements vscode.FileSystemProvider {
     });
   }
   public writeFile(uri: vscode.Uri, content: Uint8Array, options: { create: boolean; overwrite: boolean; }): void | Promise<void> {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
+      const stat = await toPromise<ssh2s.Stats>(cb => this.sftp.stat(this.relative(uri.path), cb));
       const array = new Buffer(0);
-      const stream = this.sftp.createWriteStream(this.relative(uri.path));
+      const stream = this.sftp.createWriteStream(this.relative(uri.path), { mode: stat.mode });
       stream.on('error', reject);
       stream.end(content, resolve);
     });

commit 794c2d0ad913a68626d9dfad5604fa2f606987b7

Fix bug introduced in 201a9ad (saving non-existing files)

src/sshFileSystem.ts
@@ -67,9 +67,18 @@ export class SSHFileSystem implements vscode.FileSystemProvider {
   }
   public writeFile(uri: vscode.Uri, content: Uint8Array, options: { create: boolean; overwrite: boolean; }): void | Promise<void> {
     return new Promise(async (resolve, reject) => {
-      const stat = await toPromise<ssh2s.Stats>(cb => this.sftp.stat(this.relative(uri.path), cb));
+      let mode: number | undefined;
+      try {
+        const stat = await toPromise<ssh2s.Stats>(cb => this.sftp.stat(this.relative(uri.path), cb));
+        mode = stat.mode;
+      } catch (e) {
+        if (e.message !== 'No such file') {
+          console.log(e);
+          vscode.window.showWarningMessage(`Couldn't read the permissions for '${this.relative(uri.path)}', permissions might be overwritten`);
+        }
+      }
       const array = new Buffer(0);
-      const stream = this.sftp.createWriteStream(this.relative(uri.path), { mode: stat.mode });
+      const stream = this.sftp.createWriteStream(this.relative(uri.path), { mode, flags: 'w' });
       stream.on('error', reject);
       stream.end(content, resolve);
     });