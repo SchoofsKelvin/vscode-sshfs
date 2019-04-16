27317f8031bc8dbf77ef1592d26ae0abcec4b522
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2019-04-16 19:35:51 +0200

    Fix FieldString not giving error on empty string as initial value

--- webview/src/FieldTypes/string.tsx
+++ webview/src/FieldTypes/string.tsx
@@ -2,6 +2,13 @@ import * as React from 'react';
 import { FieldBase } from './base';
 
 export class FieldString extends FieldBase<string | undefined> {
+    constructor(props: FieldString['props']) {
+        super(props);
+    }
+    public getInitialSubState({ value }: FieldString['props']): FieldString['state'] {
+        value = value || undefined;
+        return { oldValue: value, newValue: value };
+    }
     public renderInput() {
         return <input value={this.state.newValue || ''} onChange={this.onChangeEvent} />;
     }
